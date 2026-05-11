0f7ca2ee47cecd43fa4864550806fe45e488019b
adeniji-adebanjo <aeben.adebanjo@gmail.com>
2026-05-11 14:52:35 +0100

    feat: shuffle questions using Fisher-Yates algorithm on fetch

--- src/app/sermons/quiz/page.tsx
+++ src/app/sermons/quiz/page.tsx
@@ -60,6 +60,12 @@ export default function QuizPage() {
           throw new Error("No questions available");
         }
 
+        // Shuffle questions using Fisher-Yates
+        for (let i = data.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [data[i], data[j]] = [data[j], data[i]];
+        }
+
         setQuestions(data);
         setCategory(cat);
         setQuestionCount(count);
